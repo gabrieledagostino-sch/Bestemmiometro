52ab79826a2efed105b2625178fa3b2943cad2a8
Gabriele <gabry@MacBook-Air-di-Fedele.local>
2021-09-21 00:49:02 +0200

    yoo

--- tgbot.py
+++ tgbot.py
@@ -1,14 +1,30 @@
 import telebot as tb
 import dotenv 
+import requests
+
+userBest = dict()
 
 def main():
+    URL_BASE = BASE_URL = "http://bestemmie.org/api/{endpoint}?limit=900"
     token = dotenv.dotenv_values('.env','TOKEN')['TOKEN']
     bot = tb.TeleBot(token)
 
-    @bot.message_handler(commands=["test"])
+    @bot.message_handler()
     def test(message):
-        bot.reply_to(message=message, text="yo")
+        bot.reply_to(message=message, text=message.text)
+        response = requests.get(BASE_URL.format(endpoint="bestemmie"))
+        response.raise_for_status()
+        list = [a['bestemmia'] for a in response.json()['results'] if a['bestemmia'] in message.text.upper()]
+        print(list)
+        if message.from_user.id not in userBest:
+            userBest[message.from_user.id] = dict()
+        for bestemmia in list:
+            if bestemmia not in userBest[message.from_user.id]:
+                userBest[message.from_user.id][bestemmia] = 0
+            userBest[message.from_user.id][bestemmia] += 1
+        print(userBest)
     
+
     bot.polling()
 
 if __name__ == "__main__":
