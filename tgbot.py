45e258fa7dc9099b967f0ff5748de6bd6f96c5b2
Gabriele <gabry@MacBook-Air-di-Fedele.local>
2021-09-21 11:59:58 +0200

    aggiunta un po' di doc e scoperto un todo

--- tgbot.py
+++ tgbot.py
@@ -4,8 +4,18 @@ import dotenv
 import requests
 from telebot.util import MAX_MESSAGE_LENGTH
 
+RISPOSTA_NO_BESTEMMIE = 'Non stai partecipando alla gara, cresci porco di quel tuo dio'
+
+#Sistemare questi dizionari, troppo complessi
+#prolly é meglio fare una classe Utente con ste robe
 userBest = dict()
+'''
+    userBest -> {key:bestemmia, val:conto}
+'''
 userTotal = dict()
+'''
+    userTotal -> { key:chatId , val: [Nome,conta] }
+'''
 
 def main():
     BASE_URL = "http://bestemmie.org/api/{endpoint}?limit=900"
@@ -15,10 +25,16 @@ def main():
 
     @bot.message_handler(commands=['score'])
     def score(message):
+        '''
+            prende il tokenString (user id + chat id)
+            nel'if controlla se il dizionario é vuoto
+            controlla ogni bestemmia detta e quante volte nel for
+            TODO Se l'utente ha inviato un messaggio spunterá 0 come totale, si vuole mandare lo stesso il messaggio RISPOSTA_NO_BESTEMMIE
+        '''
         text = ''
         tokenString = str(message.from_user.id) + str(message.chat.id)
         if tokenString not in userBest:
-            bot.reply_to(message=message, text='Non stai partecipando alla gara, cresci porco di quel tuo dio')
+            bot.reply_to(message=message, text=RISPOSTA_NO_BESTEMMIE)
             return
         text += f'{userTotal[message.from_user.id][message.chat.id][1]} : {userTotal[message.from_user.id][message.chat.id][0]}\n'
         for key,val in sorted(userBest[tokenString].items(), key=lambda item: item[1])[::-1]:
@@ -28,8 +44,13 @@ def main():
     
     @bot.message_handler(commands=['leaderBoard'])
     def leaderboard(message):
+        '''
+            prende gli elementi del dizionario userTotal e li sorta per numero di bestemmie totale
+        '''
         text = ''
-        for key,val in userTotal.items():
+        print(userTotal.items())
+        for key,val in sorted(userTotal.items(), key = lambda item: item[1][message.chat.id][0])[::-1]:
+            print('yoo')
             text += f'{val[message.chat.id][1]} : {val[message.chat.id][0]}\n'
         bot.reply_to(message=message, text=text)
          
@@ -37,6 +58,11 @@ def main():
 
     @bot.message_handler(func=lambda item: item.text[0] != '/')
     def test(message):
+        '''
+            faccio una list comprehension con tutte le bestemmie contenute nel messaggio riconosciute su bestemmie.org
+            se l'utente non é inizializzato lo si inizializza
+            si aggiorna il dizionario delle bestemmie dette e il totale
+        '''
         list = [a['bestemmia'] for a in response if a['bestemmia'] in message.text.upper()]
         print(list)
         print(str(message.from_user.id)+str(message.chat.id))
