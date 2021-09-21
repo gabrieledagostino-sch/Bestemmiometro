bba2c0aceb8512d7f9b84938ea9f3a072bb7a772
Gabbo <gabrieledagostino50@gmail.com>
2021-09-21 23:30:52 +0200

    prove

--- tgbot.py
+++ tgbot.py
@@ -1,21 +1,23 @@
-from os import linesep
 import telebot as tb
 import dotenv 
 import requests
-from telebot.util import MAX_MESSAGE_LENGTH
 
 RISPOSTA_NO_BESTEMMIE = 'Non stai partecipando alla gara, cresci porco di quel tuo dio'
 
-#Sistemare questi dizionari, troppo complessi
-#prolly é meglio fare una classe Utente con ste robe
-userBest = dict()
 '''
-    userBest -> {key:bestemmia, val:conto}
+users -> {userId -> User}
 '''
-userTotal = dict()
-'''
-    userTotal -> { key:chatId , val: [Nome,conta] }
-'''
+users = dict()
+
+class User:
+    #chats{ chatId -> {totale : count}{bestemmia : totale};
+
+    def __init__(self, nome, chats = dict()):
+        #qui dichiaro il dizionario
+        self.chats = chats
+        self.nome = nome
+
+
 
 def main():
     BASE_URL = "http://bestemmie.org/api/{endpoint}?limit=900"
@@ -32,51 +34,72 @@ def main():
             TODO Se l'utente ha inviato un messaggio spunterá 0 come totale, si vuole mandare lo stesso il messaggio RISPOSTA_NO_BESTEMMIE
         '''
         text = ''
-        tokenString = str(message.from_user.id) + str(message.chat.id)
-        if tokenString not in userBest:
+        usId = str(message.from_user.id)
+        chatId = str(message.chat.id)
+        
+        if (usId not in users) or (chatId not in users[usId].chats) or (users[usId].chats[chatId]['totale'] == 0):
+            
             bot.reply_to(message=message, text=RISPOSTA_NO_BESTEMMIE)
             return
-        text += f'{userTotal[message.from_user.id][message.chat.id][1]} : {userTotal[message.from_user.id][message.chat.id][0]}\n'
-        for key,val in sorted(userBest[tokenString].items(), key=lambda item: item[1])[::-1]:
+        
+        user = users[usId].chats[chatId]
+
+        for key,val in sorted(user.items(), key=lambda item: item[1])[::-1]:
             text += f'{key} : {val}\n'
-        print(text)
+        
         bot.reply_to(message=message, text=text)
     
-    @bot.message_handler(commands=['leaderBoard'])
+    @bot.message_handler(commands=['leaderboard'])
     def leaderboard(message):
         '''
             prende gli elementi del dizionario userTotal e li sorta per numero di bestemmie totale
         '''
         text = ''
-        print(userTotal.items())
-        for key,val in sorted(userTotal.items(), key = lambda item: item[1][message.chat.id][0])[::-1]:
-            print('yoo')
-            text += f'{val[message.chat.id][1]} : {val[message.chat.id][0]}\n'
+        usId = str(message.from_user.id)
+        chatId = str(message.chat.id)
+        
+        unsortedDic = [us for key, us in users.items() if chatId in us.chats]
+        
+        for user in sorted(unsortedDic, key=lambda item: item.chats[chatId]['totale']):
+            text += f'{user.nome} : {user.chats[chatId]["totale"]}\n'
+
         bot.reply_to(message=message, text=text)
          
 
 
     @bot.message_handler(func=lambda item: item.text[0] != '/')
-    def test(message):
+    def bestemmioSniffer(message):
         '''
             faccio una list comprehension con tutte le bestemmie contenute nel messaggio riconosciute su bestemmie.org
             se l'utente non é inizializzato lo si inizializza
             si aggiorna il dizionario delle bestemmie dette e il totale
         '''
         list = [a['bestemmia'] for a in response if a['bestemmia'] in message.text.upper()]
-        print(list)
-        print(str(message.from_user.id)+str(message.chat.id))
-        tokenString = str(message.from_user.id)+str(message.chat.id)
-        if tokenString not in userBest:
-            userBest[tokenString] = dict()
-            userTotal[message.from_user.id] = dict()
-            userTotal[message.from_user.id][message.chat.id] = [0, message.from_user.first_name]
+        
+        usId = str(message.from_user.id)
+        chatId = str(message.chat.id)
+        user = None
+
+        print(usId)
+
+        if usId not in users:
+            users[usId] = User(message.from_user.first_name)
+        print(users[usId])
+        user = users[usId].chats
+        if chatId not in user:
+            user[chatId] = dict()
+            #qui dichiaro il totale
+            user[chatId]['totale'] = 0
+
         for bestemmia in list:
-            if bestemmia not in userBest[tokenString]:
-                userBest[tokenString][bestemmia] = 0
-            userBest[tokenString][bestemmia] += 1
-        userTotal[message.from_user.id][message.chat.id][0] += len(list)
-        print(userBest)
+            if bestemmia not in user[chatId]:
+                user[chatId][bestemmia] = 0
+            user[chatId][bestemmia] += 1
+        
+        print(user[chatId])
+        print(user)
+        #qui lo aggiorno
+        user[chatId]['totale'] += len(list)
     
     
     bot.polling()
