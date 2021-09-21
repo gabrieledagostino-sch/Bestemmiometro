fa74cc5974b2969678b13bb4fb2b65dc9cb0431b
Gabriele <gabry@MacBook-Air-di-Fedele.local>
2021-09-21 02:21:51 +0200

    v 0.1

--- tgbot.py
+++ tgbot.py
@@ -1,30 +1,58 @@
+from os import linesep
 import telebot as tb
 import dotenv 
 import requests
+from telebot.util import MAX_MESSAGE_LENGTH
 
 userBest = dict()
+userTotal = dict()
 
 def main():
-    URL_BASE = BASE_URL = "http://bestemmie.org/api/{endpoint}?limit=900"
+    BASE_URL = "http://bestemmie.org/api/{endpoint}?limit=900"
     token = dotenv.dotenv_values('.env','TOKEN')['TOKEN']
     bot = tb.TeleBot(token)
+    response = requests.get(BASE_URL.format(endpoint="bestemmie")).json()['results']
 
-    @bot.message_handler()
+    @bot.message_handler(commands=['score'])
+    def score(message):
+        text = ''
+        tokenString = str(message.from_user.id) + str(message.chat.id)
+        if tokenString not in userBest:
+            bot.reply_to(message=message, text='Non stai partecipando alla gara, cresci porco di quel tuo dio')
+            return
+        text += f'{userTotal[message.from_user.id][message.chat.id][1]} : {userTotal[message.from_user.id][message.chat.id][0]}\n'
+        for key,val in sorted(userBest[tokenString].items(), key=lambda item: item[1])[::-1]:
+            text += f'{key} : {val}\n'
+        print(text)
+        bot.reply_to(message=message, text=text)
+    
+    @bot.message_handler(commands=['leaderBoard'])
+    def leaderboard(message):
+        text = ''
+        for key,val in userTotal.items():
+            text += f'{val[message.chat.id][1]} : {val[message.chat.id][0]}\n'
+        bot.reply_to(message=message, text=text)
+         
+
+
+    @bot.message_handler(func=lambda item: item.text[0] != '/')
     def test(message):
-        bot.reply_to(message=message, text=message.text)
-        response = requests.get(BASE_URL.format(endpoint="bestemmie"))
-        response.raise_for_status()
-        list = [a['bestemmia'] for a in response.json()['results'] if a['bestemmia'] in message.text.upper()]
+        list = [a['bestemmia'] for a in response if a['bestemmia'] in message.text.upper()]
         print(list)
-        if message.from_user.id not in userBest:
-            userBest[message.from_user.id] = dict()
+        print(str(message.from_user.id)+str(message.chat.id))
+        tokenString = str(message.from_user.id)+str(message.chat.id)
+        if tokenString not in userBest:
+            userBest[tokenString] = dict()
+            userTotal[message.from_user.id] = dict()
+            userTotal[message.from_user.id][message.chat.id] = [0, message.from_user.first_name]
         for bestemmia in list:
-            if bestemmia not in userBest[message.from_user.id]:
-                userBest[message.from_user.id][bestemmia] = 0
-            userBest[message.from_user.id][bestemmia] += 1
+            if bestemmia not in userBest[tokenString]:
+                userBest[tokenString][bestemmia] = 0
+            userBest[tokenString][bestemmia] += 1
+        userTotal[message.from_user.id][message.chat.id][0] += len(list)
         print(userBest)
     
-
+    
     bot.polling()
 
 if __name__ == "__main__":
